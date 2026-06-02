dc611f289c5f0af3a0c5350ef589c259d8d72b67
tesheni <166750301+tesheni@users.noreply.github.com>
2026-06-01 22:10:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
 const letters = [
 
-"Letter 1: Happy Birthday ❤️",
+"Letter 1: Hey honey these are some letters for you!! I'm just doing this because I miss you a lot, and I might as well help myself. There only so much good I can talk about. I do infact love you more. I love always baby. My heart pangs for you everyday. Every second, mintue, hours, the days where the clock ticks, til the sun goes down. I just miss you forever, and anytime. - Love your girlfriend Tesheni <3",
 
-"Letter 2: Thank you for always making me smile.",
+"Letter 2: Thank you for always making me giggly and happy whenever we are in call. Some days are usually dull. You make everything so pretty and pink",
 
-"Letter 3: One of my favorite memories with you is...",
+"Letter 3: I enjoy the time when we were going in the botanical garden, I always loved that memory. I don't know just it was so peaceful being with you, and I guess it made me so happy, and comforted. The day was so perfect, desipte me losing my bracelt, haha. I loved making those Tiktoks with you. I love them so much, but not as much as you.,
 
-"Letter 4: You mean more to me than words can explain.",
+"Letter 4: Aw Andy your so sweet. I think about you in the song, Risk it All by Bruno Mars, like I always love that song. I sing it in my free time in the shower, and it remind mes of you. I'm currently listening to it right now, tears just form instantly. It's like my heart swells in such a good way. My shoulders heave down because everytime, I just realize I'm so in love with you.",
 
-"Letter 5: If I could relive one day with you...",
+"Letter 5: Your actually so cute when you smile, but theres this always this one time. Not always, but whenever you either like try not to get shy, your eye always dose a small 'wink' it's the most percious thing everrrrrrr !!!!!! AHHHHHH",
 
-"Letter 6",
+"Letter 6: I really love you. Agh, but you already know that hahaha LOSER. YEAH, uh huh, I called you a loser. But your my loser only teehheee",
 
-"Letter 7",
+"Letter 7: Oh my god. Ya know what your love is like, I figured it out. The feeling feels so comforting and noglastic, it remind me of the days in middle school I guess. It feels home, sure there were bad memories there, but it's slike your something that is good from my past. Even if it means you never even knew my existence. We just basically always lived the same area. Aw, my neighborhood love",
 
-"Letter 8",
+"Letter 8: You ever heard of a cute guy? Yeah, thats you. Not convinced well, I don't needa convince ya, your hot sexy, and so yummy looking, I have a thing for sweet guys :3",
 
-"Letter 9",
+"Letter 9: Your my HUSBANDDDDD AGHHHHHHHHHHHH I'm cheesing so muchhhhh!!!!!! AHHHHHHH",
 
-"Letter 10",
+"Letter 10: Oh my god, I love you so much. You remind me of goose and cat. A goose? Erm, more of cat, and a mixture of a dog personailty. As in, Chase hahaha. You guys are so similar. You are sleepy, like to eat, and shit, hahah, and mmmm, like to be petted. I like combing through your pretty curls",
 
-"Letter 11",
+"Letter 11: Agh, I remember the first time you came over. I was so so shy. You were so sweet and respectful. Awe, I love your huggies. I love the warmth you gave onto my body. Aha, even sanjeev just getting us together, props to him, propbably would've madee out without even confessing hahaha. Ahhh, wouldn't want it any other way <3",
 
-"Letter 12",
+"Letter 12: Your actually so damn sexy. Like no, you don't get it. Your squishy, strong, and are super duper strong, you can lift me. Your smart, a nerd head, a dork, and soooo freaking cuteeeeee AHHHHHH. Like thats so hot to me. Like the way you smile as friends, I always find it so endearing and sweet. Like your my sweet boy from the day and night. Forever. ",
 
-"Letter 13",
+"Letter 13: Thank you for making my last home a little more happier. Like, you made me miss it now, because of you. ",
 
-"Letter 14",
+"Letter 14: Your literally a baby puppy pitbull LIKE OMFG. Literally, just so fucking adorable, like if you seen baby pitbulls oh my LORDDDDDDDD..your literally my baby pie andy puppy :<",
 
-"Letter 15",
+"Letter 15: You know I gotta say it. I love being your mommy. Like, I love taking care of you. Wanna smother you in kisses as you fall asleep, make sure you bust so much, and then make you food to soothe out your body. I am infact a sweetheart, your sweetheart for a reason. Oh my god, I want to be your motorbicycle mommy. Like ALL PUNK and black clothes...and not to mention pay for my baby's meal whenever we go out. Agh, can't wait to get a job, and spoil my baby boy. ",
 
-"Letter 16",
+"Letter 16: I also love you being my top here and there....as muchhhhhh as much as itmakes me a little frusrated. I love it deep down. Like I enjoy it a lot. When I'm not with you, I just be this ya know, tough girl, and bla bla. But whenever I'm with you I just wanna be babied, and kissing me all day long. I just feel safe with you enough to express that side. But also I love doing the same for you I love it so so so much AHHHHH!!",
 
-"Letter 17",
+"Letter 17: Agh your so cuteeeee. Like whenever you send this face, :3, I'm like aw it's my baby boy. Like your the literal cutest ever. Agh. hehehehe i love you. ",
 
-"Letter 18",
+"Letter 18: Oh my god. The ring. I love you. Like whenever Im going somewhere and miss you. I always wear it. Like, oh no, my husband ring. Like, pretending I'm married. But why pretend when it's gonna happennnnnnn hehehehehehe :3",
 
-"Letter 19",
+"Letter 19: What do you call a cutie with curly hair? Andy Castelan. Your so supposed to laugh?? Are you not laughing?? Awww are your blushinnggg???? I can tell your blushing from reading this. Aw baby, but you are my cutie one and only. ",
 
-"Letter 20",
+"Letter 20: Bae, your literally a angry puppy at times. It's so adorable. I just wanna soothe you. ",
 
-"Letter 21",
+"Letter 21: You hate people...I hate people. I mean comeeeee onnnnnn, aren't we meant to be? But I will always love you teehee",
 
-"Letter 22",
+"Letter 22: Oh my goodness, I feel like you and me, would say this. Maybe if we stay friends,til our mid 20s and as a joke, one of us says, if we don't get married by 30, might as well marry eachother. That's our deal. And just so happens we end up falling in love. In another life, I had fall in love with you unexpectedecly...even if we are birds yes yes. OH I'm referencing from the show called Friends, SO GOOOOD AHHHHHH",
 
-"Letter 23",
+"Letter 23: Ahaha I love when I'm laughing so much in public, and you trying to calm me down hahaha. I'm realizing that, such as the train, saying, babe okay. It's so funny because I just laugh like a crazy person infront of you. But I guess I love being myself around you. I love you.",
 
-"Letter 24",
+"Letter 24: I'm so tired right now. I'm yawning, but it's worth writing to you. It makes me think that in another life, I'm looking for my prince charming, and I send letters, hoping you give me clues. For me to find you. I had write day and night just to send them. I will escape from my own castle, and find you. Because your my Andy. ",
 
-"Letter 25",
+"Letter 25: Your literally my babygirl. Just because your my hubby dosen't mean you can't be my baby girl :3 remember that",
 
-"Letter 26",
+"Letter 26: Oh my gosh. You know I loved telling my friends about having a new crush, and that is techinally you. You will always be my crush. always and forever.",
 
-"Letter 27",
+"Letter 27: You know Andy, your the light in my life. Desipte the clouds sealing the sun, you always bring them away. As the sun sets, the only light I see is you. That's how I feel when we are finally married, sleeping together. ",
 
-"Letter 28",
+"Letter 28: thank you for always choosing me honey. I choose you everyday. I love waking up, and texting you good morning, a silly voice note, or just a little...nude action hahaha. I love waiting and then seeing my Andy responded. I love it so much. I always look forward to every morning with our without being together. ",
 
 "Letter 29",
 
